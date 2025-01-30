f8eaf5d408ac9998afd28d5d671d144cda0df6cb
Tinega-Devops <oncharijohannes@gmail.com>
2025-01-30 10:39:28 +0300

    code refactoring

--- components/ChartArea.tsx
+++ components/ChartArea.tsx
@@ -1,28 +1,3 @@
-/*
-'use client';
-
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
-import { Post } from '../service/api';
-
-interface ChartAreaProps {
-  data: Post[];
-}
-
-const ChartArea: React.FC<ChartAreaProps> = ({ data }) => {
-  return (
-    <ResponsiveContainer width="100%" height={400}>
-      <BarChart data={data}>
-        <XAxis dataKey="title" hide />
-        <YAxis />
-        <Tooltip />
-        <Bar dataKey="userId" fill="#3182ce" />
-      </BarChart>
-    </ResponsiveContainer>
-  );
-};
-
-export default ChartArea;
-*/
 "use client";
 
 import { useGetCryptoDataQuery } from "../store/api";
